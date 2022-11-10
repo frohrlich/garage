aef49bdae3149641d2cf2125df11ce99f9960d5d
Flae-Flo <floriane.mathonnat@orange.fr>
2022-11-10 17:23:56 +0100

    front-end page accueil

--- index.php
+++ index.php
@@ -1,289 +1,172 @@
 <!DOCTYPE html>
 <html lang="fr">
-  <head>
-    <!-- Basic -->
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <!-- Mobile Metas -->
-    <meta
-      name="viewport"
-      content="width=device-width, initial-scale=1, shrink-to-fit=no"
-    />
-    <!-- Site Metas -->
-    <meta name="keywords" content="" />
-    <meta name="description" content="" />
-    <meta name="author" content="" />
-    <link rel="shortcut icon" href="images/favicon.png" type="image/x-icon" />
-    <title>Garage Pistons & Boulons</title>
 
-    <!-- bootstrap core css -->
-    <!-- <link rel="stylesheet" type="text/css" href="css/bootstrap.css" /> -->
+<head>
+  <!-- Basic -->
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <!-- Mobile Metas -->
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <!-- Site Metas -->
+  <meta name="keywords" content="" />
+  <meta name="description" content="" />
+  <meta name="author" content="" />
+  <link rel="shortcut icon" href="images/favicon.png" type="image/x-icon" />
+  <title>Garage Pistons & Boulons</title>
 
-    <!--owl slider stylesheet -->
-    <link
-      rel="stylesheet"
-      type="text/css"
-      href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css"
-    />
-    <!-- font awesome style -->
-    <link href="css/font-awesome.min.css" rel="stylesheet" />
+  <!-- bootstrap core css -->
+  <!-- <link rel="stylesheet" type="text/css" href="css/bootstrap.css" /> -->
 
-    <!-- Custom styles for this template -->
-    <link href="css/style.min.css" rel="stylesheet" />
-    <!-- responsive style -->
-    <link href="css/responsive.css" rel="stylesheet" />
-  </head>
+  <!--owl slider stylesheet -->
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
+  <!-- font awesome style -->
+  <link href="css/font-awesome.min.css" rel="stylesheet" />
 
-  <body>
-    <div class="hero_area">
-    <!-- header section -->
-    <?php include 'header.php' ?>
+  <!-- Custom styles for this template -->
+  <link href="css/style.min.css" rel="stylesheet" />
+  <!-- responsive style -->
+  <link href="css/responsive.css" rel="stylesheet" />
+</head>
 
-    <!-- Navigation bar -->
-    <?php include 'navbar.php' ?>
+<body>
+  <!-- header section -->
+  <?php include 'header.php' ?>
 
-      <!-- slider section -->
-      <section class="slider_section position-relative">
+  <!-- Navigation bar -->
+  <?php include 'navbar.php' ?>
 
-        <div class="slider_container">
-          <div
-            id="carouselExampleIndicators"
-            class="carousel slide"
-            data-ride="carousel"
-          >
-            <div class="carousel-inner">
-              <div class="carousel-item active">
-                <div class="container">
-                  <div class="row">
-                    <div class="col-md-6">
-                      <div class="detail-box">
-                        <h1>
-                          We Care For <br />
-                          Your Car
-                        </h1>
-                        <p>
-                          Lorem ipsum dolor sit amet, consectetur adipiscing
-                          elit, sed do eiusmod tempor incididunt ut labore et
-                          dolore magna aliqua. Ut enim ad minim veniam
-                        </p>
-                        <div class="btn-box">
-                          <a href="" class="btn-1"> Read More </a>
-                          <a href="" class="btn-2"> Contact Us </a>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="col-md-6">
-                      <div class="img-box">
-                        <img src="images/slider-img.jpg" alt="" />
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div class="carousel-item">
-                <div class="container">
-                  <div class="row">
-                    <div class="col-md-6">
-                      <div class="detail-box">
-                        <h1>
-                          We Care For <br />
-                          Your Car
-                        </h1>
-                        <p>
-                          Lorem ipsum dolor sit amet, consectetur adipiscing
-                          elit, sed do eiusmod tempor incididunt ut labore et
-                          dolore magna aliqua. Ut enim ad minim veniam
-                        </p>
-                        <div class="btn-box">
-                          <a href="" class="btn-1"> Read More </a>
-                          <a href="" class="btn-2"> Contact Us </a>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="col-md-6">
-                      <div class="img-box">
-                        <img src="images/slider-img.jpg" alt="" />
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div class="carousel-item">
-                <div class="container">
-                  <div class="row">
-                    <div class="col-md-6">
-                      <div class="detail-box">
-                        <h1>
-                          We Care For <br />
-                          Your Car
-                        </h1>
-                        <p>
-                          Lorem ipsum dolor sit amet, consectetur adipiscing
-                          elit, sed do eiusmod tempor incididunt ut labore et
-                          dolore magna aliqua. Ut enim ad minim veniam
-                        </p>
-                        <div class="btn-box">
-                          <a href="" class="btn-1"> Read More </a>
-                          <a href="" class="btn-2"> Contact Us </a>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="col-md-6">
-                      <div class="img-box">
-                        <img src="images/slider-img.jpg" alt="" />
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="container">
-              <ol class="carousel-indicators">
-                <li
-                  data-target="#carouselExampleIndicators"
-                  data-slide-to="0"
-                  class="active"
-                ></li>
-                <li
-                  data-target="#carouselExampleIndicators"
-                  data-slide-to="1"
-                ></li>
-                <li
-                  data-target="#carouselExampleIndicators"
-                  data-slide-to="2"
-                ></li>
-              </ol>
-            </div>
-          </div>
-        </div>
-      </section>
-      <!-- end slider section -->
-    </div>
+  <!-- Banner section -->
+  <section class="banner_section ">
 
-    <!-- about section -->
-
-    <section class="about_section layout_padding">
+    <div class="banner_container">
       <div class="container">
         <div class="row">
-          <div class="col-lg-6">
-            <div class="img-box">
-              <img src="images/about-img.jpg" class="box_img" alt="about img" />
-            </div>
-          </div>
-          <div class="col-lg-6">
-            <div class="detail-box pr-md-2">
-              <div class="heading_container">
-                <h2 class="">About Us</h2>
-              </div>
-              <p class="detail_p_mt">
-                Facere corporis quis non quas assumenda repudiandae rem, at
-                fugit rerum fugiat deserunt sequi necessitatibus accusantium
-                doloribus veniam quo consequatur cumque illum nostrum
-                consectetur nobis impedit deleniti.
+          <div class="col-md-6">
+            <div class="detail-box">
+              <h1>
+                Bienvenue au garage <br />
+                Pistons & Boulons !
+              </h1>
+              <p>
+                Spécialisé en véhicules Dacia depuis 2011
               </p>
               <p>
-                Nisi quod sequi minima? Saepe cupiditate, voluptates temporibus
-                vel magni id laborum odio quis dicta quaerat tempora nemo. Atque
-                ratione numquam fugit ullam eos illo adipisci dicta dignissimos
-                cumque quasi!
+                Venez nous rencontrer au : <br>
+                <span>
+                  1 Quai des Mégisseries, <br>
+                  87200 Saint-Junien
+                </span>
               </p>
-              <a href="about.php" class=""> Read More </a>
+              <p>
+                Téléphone : <br>
+                <span> 05 55 02 10 23 </span>
+              </p>
+              <p>
+                Horaires : <br>
+                Lundi au vendredi de <span> 8h30 à 19h </span> <br>
+                Le samedi de <span> 8h30 à 12h30 </span> <br>
+                Fermé le dimanche et les jours fériés
+              </p>
+              <div class="btn-box">
+                <a href="contact.php" class="btn-1"> Nous contacter </a>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 d-none d-md-block">
+            <div class="img-box">
+              <img src="images/accueil-kat-sazonova-i8DMKtMyfWM-unsplash.jpg" alt="Photo de mécanique" />
             </div>
           </div>
         </div>
       </div>
-    </section>
+    </div>
+  </section>
 
-    <!-- about section ends -->
+  <!-- service section -->
 
-    <!-- service section -->
-
-    <section class="service_section layout_padding">
-      <div class="container">
-        <div class="heading_container heading_center">
-          <h2 class="">Services We Offer</h2>
-          <p class="col-lg-8 px-0">
-            If you are going to use a passage of Lorem Ipsum, you need to be
-            sure there isn't anything believable. If you are going to use a
-            passage of Lorem Ipsum, you need to be sure there isn't anything
-          </p>
-        </div>
-        <div class="row">
-          <div class="col-md-6 col-lg-4 mx-auto">
-            <div class="box">
-              <div class="img-box">
-                <img src="images/s1.jpg" alt="" />
-              </div>
-              <div class="detail-box">
-                <h3>Car Inspection</h3>
-                <p>
-                  when looking at its layout. The point of using Lorem Ipsum is
-                  that it has a more-or-less normal
-                </p>
-                <a href="">
-                  Read More
-                  <i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i>
-                </a>
-              </div>
+  <section class="service_section layout_padding">
+    <div class="container">
+      <div class="heading_container heading_center">
+        <h2> Découvrir nos prestations </h2>
+      </div>
+      <div class="row">
+        <div class="col-md-6 col-lg-4 mx-auto">
+          <div class="box">
+            <div class="img-box">
+              <img src="images/s1.jpg" alt="" />
             </div>
-          </div>
-          <div class="col-md-6 col-lg-4 mx-auto">
-            <div class="box">
-              <div class="img-box">
-                <img src="images/s2.jpg" alt="" />
-              </div>
-              <div class="detail-box">
-                <h3>Car Cleaning</h3>
-                <p>
-                  when looking at its layout. The point of using Lorem Ipsum is
-                  that it has a more-or-less normal
-                </p>
-                <a href="">
-                  Read More
-                  <i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i>
-                </a>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-6 col-lg-4 mx-auto">
-            <div class="box">
-              <div class="img-box">
-                <img src="images/s3.jpg" alt="" />
-              </div>
-              <div class="detail-box">
-                <h3>Car Repair</h3>
-                <p>
-                  when looking at its layout. The point of using Lorem Ipsum is
-                  that it has a more-or-less normal
-                </p>
-                <a href="">
-                  Read More
+            <div class="detail-box">
+              <h3>Spécialisé véhicules Dacia</h3>
+              <p>
+                Notre garage est avant tout un centre technique pour la réparation et l’entretien de votre Dacia.
+              </p>
+              <div>
+                <a href="about.php">
+                  En savoir plus
                   <i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i>
                 </a>
               </div>
             </div>
           </div>
         </div>
-        <div class="btn-box">
-          <a href=""> View More </a>
+        <div class="col-md-6 col-lg-4 mx-auto">
+          <div class="box">
+            <div class="img-box">
+              <img src="images/s2.jpg" alt="" />
+            </div>
+            <div class="detail-box">
+              <h3>Accessoires et pièces détachées</h3>
+              <p>
+                Vitre, rétroviseur, <br> nous disposons de nombreuses pièces détachées.
+              </p>
+              <div>
+                <a href="about.php">
+                  En savoir plus
+                  <i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6 col-lg-4 mx-auto">
+          <div class="box">
+            <div class="img-box">
+              <img src="images/s3.jpg" alt="" />
+            </div>
+            <div class="detail-box">
+              <h3>Pose et entretien de vos boîtiers éthanol</h3>
+              <p>
+                Pour toutes vos voitures essence, nous vous aiderons à passer au bioéthanol.
+              </p>
+              <div>
+                <a href="about.php">
+                  En savoir plus
+                  <i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i>
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-    </section>
+      <div class="btn-box">
+        <a href="about.php"> Toutes nos prestations </a>
+      </div>
+    </div>
+  </section>
 
-    <!-- service section ends -->
+  <!-- footer section -->
+  <?php include 'footer.php' ?>
 
-    <!-- footer section -->
-    <?php include 'footer.php' ?>
+  <!-- jQery -->
+  <script src="node_modules\jquery\dist\jquery.min.js"></script>
+  <!-- popper js -->
+  <script src="node_modules\popper.js\dist\umd\popper.min.js"></script>
+  <!-- bootstrap js -->
+  <script src="node_modules\bootstrap\dist\js\bootstrap.min.js"></script>
+  <!-- owl slider -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+  <!-- custom js -->
+  <script src="js/custom.js"></script>
+</body>
 
-    <!-- jQery -->
-    <script src="node_modules\jquery\dist\jquery.min.js"></script>
-    <!-- popper js -->
-    <script src="node_modules\popper.js\dist\umd\popper.min.js"></script>
-    <!-- bootstrap js -->
-    <script src="node_modules\bootstrap\dist\js\bootstrap.min.js"></script>
-    <!-- owl slider -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
-    <!-- custom js -->
-    <script src="js/custom.js"></script>
-  </body>
 </html>
