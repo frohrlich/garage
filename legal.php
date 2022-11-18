c3c6728993ef657279ea4dd638a9c72e00580958
Dilki087 <adilbahri87@gmail.com>
2022-11-18 14:36:41 +0100

    plan du site

---
+++ legal.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <!-- Basic -->
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <!-- Mobile Metas -->
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, shrink-to-fit=no"
+    />
+    <!-- Site Metas -->
+    <meta name="keywords" content="" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <link rel="shortcut icon" href="images/favicon.png" type="image/x-icon" />
+    <title>Services - Garage Pistons & Boulons</title>
+
+    <!-- bootstrap core css -->
+    <!-- <link rel="stylesheet" type="text/css" href="css/bootstrap.css" /> -->
+
+    <!--owl slider stylesheet --> 
+    <link
+      rel="stylesheet"
+      type="text/css"
+      href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css"
+    />
+    <!-- font awesome style -->
+    <link href="css/font-awesome.min.css" rel="stylesheet" />
+
+    <!-- Custom styles for this template -->
+    <link href="css/style.min.css" rel="stylesheet" />
+    <!-- responsive style -->
+    <link href="css/responsive.css" rel="stylesheet" />
+  </head>
+
+  <body>
+    <div class="hero_area">
+      <!-- header section -->
+      <?php include 'header.php'; ?>
+
+      <!-- Navigation bar -->
+      <?php include 'navbar.php'; ?>
+
+    <!-- about section -->
+
+   <div class="container">
+                <h1 class="text-center m-5">Mentions légales</h1>
+             
+              <p class="detail_p_mt">
+              Conformément aux dispositions des Articles 6-III et 19 de la Loi n°2004-575 du 21 juin 2004 
+              pour la Confiance dans l’économie numérique, dite L.C.E.N., il est porté à la connaissance des utilisateurs et visiteurs,
+               ci-après l""Utilisateur", du site www.pistonsetboulons.fr , ci-après le "Site", les présentes mentions légales.
+              La connexion et la navigation sur le Site par l’Utilisateur implique acceptation 
+              intégrale et sans réserve des présentes mentions légales.
+             Ces dernières sont accessibles sur le Site à la rubrique « Mentions légales ».
+            </p>
+            <h2>Article 1 - l'editeur</h2>
+              <p>
+              L'édition du Site est assurée par Web agence sarl au capital de 100 euros, immatriculée au Registre du Commerce et des Sociétés de limoges sous le numéro 123453313421 dont le siège social est situé au 12 rue de la route 87000 limoges ,
+              Numéro de téléphone 0555103223, </br>
+              Adresse e-mail : contact@webagence.com.</br>
+            N° de TVA intracommunautaire : 1234253848234</br>
+             Le Directeur de la publication est Thor ragnarok</br>
+              ci-après l'"Editeur".
+</p>
+
+              <h2> Article 2 - L'hebergeur</h2>
+              <p> L'hébergeur du Site est la société Piston & Boulons, dont le siege social est situé a Saint Junien,
+                 avec le numéro de téléphone: 05 55 02 10 23
+              </p>
+              <h2> Article 3 - Acces au Site</h2>
+              <p>Le Site est accessible en tout endroit, 7j/7, 24h/24 sauf cas de force majeure,
+                 interruption programmée ou non et pouvant découlant d’une nécessité de maintenance.
+   </br>
+                 En cas de modification, interruption ou suspension du Site, l'Editeur ne saurait être tenu responsable.
+</p>
+              <h2> Article 4- Collecte des données </h2>
+<p>Le Site assure à l'Utilisateur une collecte et un traitement d'informations personnelles
+     dans le respect de la vie privée conformément à la loi n°78-17 du 6 janvier 1978 relative à l'informatique, 
+     aux fichiers et aux libertés.
+     En vertu de la loi Informatique et Libertés, en date du 6 janvier 1978, l'Utilisateur dispose d'un droit d'accès, de rectification, de suppression et d'opposition de ses données personnelles. L'Utilisateur exerce ce droit :
+· par mail à l'adresse email
+· via son espace personnel ;
+Toute utilisation, reproduction, diffusion, commercialisation,
+ modification de toute ou partie du Site, sans autorisation de l’Editeur 
+ est prohibée et pourra entraînée des actions et poursuites judiciaires telles 
+ que notamment prévues par le Code de la propriété intellectuelle et le Code civil.
+     </p>
+
+     </div>
+           
+
+    <!-- footer section -->
+    <?php include 'footer.php'; ?>
+
+    <!-- jQery -->
+    <script src="node_modules\jquery\dist\jquery.min.js"></script>
+    <!-- popper js -->
+    <script src="node_modules\popper.js\dist\umd\popper.min.js"></script>
+    <!-- bootstrap js -->
+    <script src="node_modules\bootstrap\dist\js\bootstrap.min.js"></script>
+    <!-- owl slider -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+    <!-- custom js -->
+    <script src="js/custom.js"></script>
+   
+     </body>
+</html>
